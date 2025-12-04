769868029fd383ecc2a256fbfa200bd1a458d760
SuchanMin <suchanwongsakun@gmail.com>
2025-12-04 09:12:27 +0700

    Add files via upload

---
+++ index01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Php Array ( เรียนวันที่ 4/12/2568 )</title>
+</head>
+<body>
+    <h1>ทดสอบ Index Array </h1>
+    <?php
+    $cars = array("Volvo","BMW","Toyota");
+    echo "I like". $cars[0].",".$cars[1]."and".$cars[2].".";
+
+    ?>
+    
+</body>
+</html>
